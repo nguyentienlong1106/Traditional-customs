4178f46bbe9ebf982433fd8dc7df3e05f42ca1af
nguyentienlong1106 <tienlong1106@gmail.com>
2024-05-08 04:56:45 +0300

    fix

--- app/api/products/seed/route.ts
+++ app/api/products/seed/route.ts
@@ -1,21 +1,17 @@
-import data from '@/lib/data'
-import dbConnect from '@/lib/dbConnect'
-import ProductModel from '@/lib/models/ProductModel'
-import UserModel from '@/lib/models/UserModel'
-import { NextRequest, NextResponse } from 'next/server'
+import data from "@/lib/data";
+import dbConnect from "@/lib/dbConnect";
+import ProductModel from "@/lib/models/ProductModel";
+import UserModel from "@/lib/models/UserModel";
+import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (request: NextRequest) => {
-  const { users, products } = data
-  await dbConnect()
-  await UserModel.deleteMany()
-  await UserModel.insertMany(users)
-
-  await ProductModel.deleteMany()
-  await ProductModel.insertMany(products)
+  const { users } = data;
+  await dbConnect();
+  await UserModel.deleteMany();
+  await UserModel.insertMany(users);
 
   return NextResponse.json({
-    message: 'seeded successfully',
+    message: "seeded successfully",
     users,
-    products,
-  })
-}
+  });
+};
